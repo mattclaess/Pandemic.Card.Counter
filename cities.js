82bd7ed9e68d249ab8c491ee078964a1f2275a02
Alice Zhang <ax.zhang@hotmail.com>
2020-01-10 00:48:28 -0500

    added .less file and cities master list in separate js. needs to be updated, don't remember exact values

---
+++ cities.js
@@ -0,0 +1,64 @@
+// what cards we know are in the deck
+// cityname: {
+//    total: number
+//    color: blue | yellow | black | red ?
+// }
+const CITY_CARDS_IN_INFECTION_DECK = {
+    // populate
+    "denver": {
+        color: "blue",
+        total: 2
+    },
+    "atlanta": {
+        color: "blue",
+        total: 2
+    },
+    "new york": {
+        color: "blue",
+        total: 3
+    },
+    "washington": {
+        color: "blue",
+        total: 3
+    },
+    "london": {
+        color: "blue",
+        total: 3
+    },
+    "chicago": {
+        color: "blue",
+        total: 3
+    },
+    "jacksonville": {
+        color: "yellow",
+        total: 3
+    },
+    "lima": {
+        color: "yellow",
+        total: 3
+    },
+    "sao paulo": {
+        color: "yellow",
+        total: 3
+    },
+    "buenos aires": {
+        color: "yellow",
+        total: 3
+    },
+    "lagos": {
+        color: "yellow",
+        total: 3
+    },
+    "tripoli": {
+        color: "black",
+        total: 3
+    },
+    "cairo": {
+        color: "black",
+        total: 3
+    },
+    "istanbul": {
+        color: "black",
+        total: 3
+    }
+  }
